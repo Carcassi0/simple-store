8fc01095cc4af5c4dd7882803c0524cc17634b54
WEIUIJONG <dnldmlwhd@ajou.ac.kr>
2024-08-29 20:59:10 +0900

    Changes to be committed: 	modified:   client/vite.config.js 	modified:   vercel.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Vercel 설정과 맞추기 위해 기본값 사용
+    outDir: 'dist',
   },
 });
